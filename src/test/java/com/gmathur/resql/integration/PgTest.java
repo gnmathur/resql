8b6b2e2491020cb368909a2a1d47edbc142e5111
Gaurav Mathur <gmathur@gurukul>
2022-02-06 23:16:55 -0800

    Added JDBC connection

--- src/test/java/com/gmathur/resql/integration/PgTest.java
+++ src/test/java/com/gmathur/resql/integration/PgTest.java
@@ -9,8 +9,10 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.Arrays;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
 public class PgTest {
@@ -19,14 +21,17 @@ public class PgTest {
     private static final String POSTGRES_PASSWORD = "resql";
     private static final String POSTGRES_DB = "resql";
     private static final String POSTGRES_NETWORK_ALIASES = "resqlpgnw";
-    private static final Integer POSTGRES_EXPOSED_PORT = 54321;
+    // TODO make PG work on a non-default port
+    //private static final Integer POSTGRES_EXPOSED_PORT = 54321;
     // ??? This most likely is not needed as there are no other containers to talk to
     private static Network resqlNetwork = Network.newNetwork();
+    private static JdbcHandle jdbcHandle;
 
     @Container
-    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:11.1")
+    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:13-alpine")
             .withDatabaseName(POSTGRES_DB)
-            .withExposedPorts(POSTGRES_EXPOSED_PORT)
+            // TODO make PG work on a non-default port
+      //      .withExposedPorts(POSTGRES_EXPOSED_PORT)
             .withNetwork(resqlNetwork)
             .withNetworkAliases(POSTGRES_NETWORK_ALIASES)
             .withUsername(POSTGRES_USER)
@@ -34,10 +39,17 @@ public class PgTest {
 
     @BeforeAll
     public static void init() {
-        LOGGER.info(postgreSQLContainer.getExposedPorts().toString());
+        LOGGER.info("Initializing PG JDBC connection to {}", postgreSQLContainer.getJdbcUrl());
+        jdbcHandle = new JdbcHandle(postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword());
     }
 
     @Test
-    public void testThis() {
+    public void dummyQueryTest() throws SQLException {
+        ResultSet rs = jdbcHandle.doQuery("Select 1");
+        while(rs.next()) {
+            assertEquals(1, rs.getInt(1));
+        }
     }
 }
